57da48d5a426e8201b762442ba242d0f2074aa2b
Alex <Alexander.Stolpe@gmail.com>
2025-05-13 22:35:37 +0200

    Small uppdate to the Slide pages form

--- frontend/src/app/slides/slides.component.ts
+++ frontend/src/app/slides/slides.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { TextareaModule } from 'primeng/textarea';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-slides',
-  imports: [],
+  imports: [
+    InputTextModule,
+    ReactiveFormsModule,
+    FloatLabelModule,
+    TextareaModule,
+    ButtonModule
+  ],
   templateUrl: './slides.component.html',
   styleUrl: './slides.component.scss'
 })
 export class SlidesComponent {
+  slideForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl('')
+  });
 
+  onSubmit() {
+    
+  }
 }
